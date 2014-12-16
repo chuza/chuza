586ba15c8fa3c6edda3679e90fc30ad23c7673ea
Manel Vilar <manelvf@gmail.com>
2014-12-16 21:35:02 +0100

    improved script

--- www/scripts-1246/aede.php
+++ www/scripts-1246/aede.php
@@ -6,18 +6,36 @@ include_once(mnminclude.'log.php');
 include_once(mnminclude.'ban.php');
 include_once(mnminclude.'canon-AEDE.php');
 
-$s = "SELECT * FROM `links` WHERE link_status != 'abuse' LIMIT 1000 ";
-$r = $db->get_results($s);
+$counter = 0;
 
-echo "<pre>";
-foreach ($r as $row) {
-	if (Canon_AEDE::remove_shit($row->link_url)) {
-		$s="UPDATE links SET link_status='abuse' WHERE link_id=".$row->link_id;
-		echo $s."\n";
-		$r = $db->query($s);
-		echo $r;
+function kill_me($from, $to) {
+	global $db, $counter;
+
+	echo "batch $from, $to \n";
+	$s = "SELECT * FROM `links` WHERE link_status != 'autodiscard' LIMIT $from, $to";
+	$r = $db->get_results($s);
+
+	echo "<pre>";
+	foreach ($r as $row) {
+		if (Canon_AEDE::remove_shit($row->link_url)) {
+			$s="UPDATE links SET link_status='autodiscard' WHERE link_id=".$row->link_id;
+			echo $s." = ";
+			$r = $db->query($s);
+			echo $r."\n";
+			$counter++;
+		}
 	}
 }
 
+$s = "UPDATE FROM `links` SET link_status='published' WHERE link_status = 'autodiscard' ";
+$r = $db->query($s);
+
+echo 'start<br>';
+for ($k=0; $k<10000; $k++) {
+	$x = $k * 5000;
+	kill_me($x, $x+5000);
+}
+
+echo "$counter noticias mudadas\n";
 echo "Fin do script";
 
